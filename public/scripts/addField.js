204a80be901bb16415d6013f8dbc9ced284ec3c5
biagavirete <64283019+biagavirete@users.noreply.github.com>
2020-08-08 16:23:00 -0300

    trilha_discovery_ok
    Conclusão nlw#2

---
+++ public/scripts/addField.js
@@ -0,0 +1,23 @@
+// procurar o botão
+document.querySelector("#add-time")
+// quando clicar no botão
+.addEventListener('click', cloneField)
+
+// executar uma ação
+function cloneField() { 
+    // duplicar os campos (que campos?)
+    const newFieldContainer = document.querySelector('.schedule-item').cloneNode(true) // boolean: true or false
+
+    // pegar os campos (que campos?)
+    const fields = newFieldContainer.querySelectorAll('input')
+
+    // para cada campo, limpar
+    fields.forEach(function(field) {
+        // pegar o field do momento e o limpar
+        field.value = ""
+    })
+
+    // colocar na página (onde?)
+    document.querySelector('#schedule-items').appendChild(newFieldContainer)
+
+}
